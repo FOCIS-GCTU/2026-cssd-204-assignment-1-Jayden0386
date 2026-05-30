daf7987c6049a49070bb1c2ec813eadb798be368
Jayden0386 <Princekwakufiador31@gmail.com>
2026-05-30 23:07:00 +0000

    Add files via upload

--- initials.py
+++ initials.py
@@ -1,7 +1,36 @@
+# File: initials.py
+# Description: Print out my initials.
+# Assignment Number: 1
+#
+# Name: PRINCE KWAKU FIADOR
+# STUDENT ID: 2425402851
+# Email: 2425402852@live.gctu.edu.gh
+# Grader:Augustus Buckman
+#
+# On my honor,Augustus Buckman, this programming assignment is my own work
+# and I have not provided this code to any other student.
 
 def main():
-  pass #code goes here
 
+
+    print()
+    print("...PKF")
+    print()
+
+    print("...PPPPPPPPPPPP..........KK........KK..............FFFFFFFFFFFF")
+    print("...PP........PP..........KK......KK................FF..........")
+    print("...PP........PP..........KK....KK..................FF..........")
+    print("...PPPPPPPPPPPP..........KK..KK....................FFFFFFFFFFFF")
+    print("...PP....................KKKK......................FF..........")
+    print("...PP....................KK..KK....................FF..........")
+    print("...PP....................KK....KK..................FF..........")
+    print("...PP....................KK......KK................FF..........")
+    print("...PP....................KK........KK..............FF..........")
+    print("...PP..........**........KK..........KK....**......FF..........")
+
+
+print()
 
 
 main()
+
